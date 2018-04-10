b646d4a56e2471bc0fa0df22cf5a427538688658
tujiahua <137112104@qq.com>
2018-04-10 20:10:16 +0800

    update

--- chaincode/fabblack/go/fabblack.go
+++ chaincode/fabblack/go/fabblack.go
@@ -16,11 +16,11 @@ import (
 )
 
 // Define the BlacklistContract structure
-type BlacklistContract struct {
+type demoContract struct {
 }
 
 // Define the Blacklist structure, with 3 properties.
-type Blacklist struct {
+type demo struct {
 	Index     int         `json:"Index"`
 	Timestamp interface{} `json:"Timestamp"`
 	BLHash    string      `json:"BLHash"`
@@ -30,35 +30,35 @@ type Blacklist struct {
  * The Init method is called when the Smart Contract "fabblack" is instantiated by the blockchain network
  * Best practice is to have any Ledger initialization in separate function -- see initLedger()
  */
-func (B *BlacklistContract) Init(stub shim.ChaincodeStubInterface) pb.Response {
+func (B *demoContract) Init(stub shim.ChaincodeStubInterface) pb.Response {
 	return shim.Success(nil)
 }
 
-func (B *BlacklistContract) Invoke(stub shim.ChaincodeStubInterface) pb.Response {
+func (B *demoContract) Invoke(stub shim.ChaincodeStubInterface) pb.Response {
 	// Retrieve the requested Smart Contract function and arguments
 	function, args := stub.GetFunctionAndParameters()
 
 	// Route to the appropriate handler function to interact with the ledger appropriately
 	switch function {
-	case "queryBlacklist":
-		return B.queryBlacklist(stub, args)
-	case "CreateBlacklist":
-		return B.CreateBlacklist(stub, args)
+	case "querydemo":
+		return B.querydemo(stub, args)
+	case "Createdemo":
+		return B.Createdemo(stub, args)
 	default:
 		return shim.Error("no such function!")
 	}
 }
 
 //query blacklist by key(That is "Index")
-func (B *BlacklistContract) queryBlacklist(stub shim.ChaincodeStubInterface, Args []string) pb.Response {
+func (B *demoContract) querydemo(stub shim.ChaincodeStubInterface, Args []string) pb.Response {
 	if len(Args) != 1 {
 		return shim.Error("Incorrect number of arguments. Expecting 1")
 	}
-	blacklist, _ := stub.GetState(Args[0])
-	return shim.Success(blacklist)
+	demo, _ := stub.GetState(Args[0])
+	return shim.Success(demo)
 }
 
-func (B *BlacklistContract) CreateBlacklist(stub shim.ChaincodeStubInterface, Args []string) pb.Response {
+func (B *demoContract) Createdemo(stub shim.ChaincodeStubInterface, Args []string) pb.Response {
 	if len(Args) != 2 {
 		return shim.Error("Incorrect number of arguments. Expecting 3")
 	}
@@ -67,15 +67,22 @@ func (B *BlacklistContract) CreateBlacklist(stub shim.ChaincodeStubInterface, Ar
 		return shim.Error(err.Error())
 	}
 	index, _ := strconv.Atoi(Args[0])
-	var blacklist = Blacklist{Index: index, Timestamp: time, BLHash: Args[1]}
-	blacklistAsBytes, _ := json.Marshal(blacklist)
-	stub.PutState(Args[0], blacklistAsBytes)
+	var demo = demo{Index: index, Timestamp: time, BLHash: Args[1]}
+	demoAsBytes, _ := json.Marshal(demo)
+	stub.PutState(Args[0], demoAsBytes)
 
 	return shim.Success(nil)
 }
 
+func (B *demoContract) GetDataFromIPFS(stub shim.ChaincodeStubInterface, Args []string) pb.Response {
+	if len(Args) != 1 {
+		return shim.Error("Incorrect number of arguments. Expecting 1")
+	}
+
+}
+
 func main() {
-	err := shim.Start(new(BlacklistContract))
+	err := shim.Start(new(demoContract))
 	if err != nil {
 		fmt.Println("Error create new Smart Contract: %s", err)
 	}
